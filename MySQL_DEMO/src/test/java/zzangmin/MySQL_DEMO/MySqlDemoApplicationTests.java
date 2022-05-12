5010fcfeb33cd77c202fc53a88b94789f6b4cea7
leezzangmin <ckdals12345678@gmail.com>
2022-05-12 14:59:33 +0900

    spring demo project init

---
+++ MySQL_DEMO/src/test/java/zzangmin/MySQL_DEMO/MySqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package zzangmin.MySQL_DEMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
