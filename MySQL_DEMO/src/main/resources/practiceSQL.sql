9b53becee8fa2a7ecb89766f5aeb573f4678044e
leezzangmin <ckdals12345678@gmail.com>
2022-05-17 00:03:47 +0900

    인덱스 스킵 예제

--- MySQL_DEMO/src/main/resources/practiceSQL.sql
+++ MySQL_DEMO/src/main/resources/practiceSQL.sql
@@ -1,1 +1,9 @@
 SET optimizer_switch='skip_scan=on';
+ALTER TABLE employees ADD INDEX ix_gender_birthdate (gender,birth_date);
+
+select gender, birth_date from employees WHERE birth_date>='1965-02-01';
+----------------------------------------------------------------------------------------
+------------------------explain---------------------------------------------------------
+----------------------------------------------------------------------------------------
+select gender, birth_date from employees WHERE gender='M' AND birth_date>='1965-02-01';
+select gender, birth_date from employees WHERE gender='F' AND birth_date>='1965-02-01';
